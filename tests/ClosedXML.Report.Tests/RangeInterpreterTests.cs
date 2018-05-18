7fdf0be4f50c6016d6c8d06bbde6a3272daafd11
Aleksei <pankraty@gmail.com>
2018-05-18 16:44:23 +0400

    Failing test added

--- tests/ClosedXML.Report.Tests/RangeInterpreterTests.cs
+++ tests/ClosedXML.Report.Tests/RangeInterpreterTests.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using ClosedXML.Excel;
+using FluentAssertions;
+using System.Collections.Generic;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -20,6 +22,38 @@ namespace ClosedXML.Report.Tests
                     wb.Worksheet(2).Cell("A2").IsEmpty().Should().BeTrue();
                     wb.Worksheet(3).Cell("B4").GetString().Should().NotContain("<<OnlyValues>>");
                 });
+        }
+
+        [Fact]
+        public void DoNotEvaluateFormulaOnTagsParsing()
+        {
+            using (var wb = new XLWorkbook())
+            {
+                var ws1 = wb.AddWorksheet("Sheet1");
+                var ws2 = wb.AddWorksheet("Sheet2");
+
+                ws1.FirstCell().FormulaA1 = "=VLOOKUP(\"Bob\", Sheet2!B:D, 3, FALSE)";
+                ws2.Cell("B2").Value = "{{item.Name}}";
+                ws2.Cell("C2").Value = "{{item.Count}}";
+                ws2.Cell("D2").Value = "&=C2*10";
+                ws2.Range("A2:D3").AddToNamed("Items");
+
+                var template = new XLTemplate(wb);
+                template.AddVariable("Items", GenerateItems());
+                template.Generate();
+
+                ws1.FirstCell().Value.Should().Be(20.0);
+            }
+
+            IEnumerable<object> GenerateItems()
+            {
+                return new List<object>
+                {
+                    new { Name = "Alice", Count = 1 },
+                    new { Name = "Bob", Count = 2 },
+                    new { Name = "Carl", Count = 3 },
+                };
+            }
         }
     }
 }
