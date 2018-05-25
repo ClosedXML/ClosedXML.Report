88e5bb34ded2889fe44292b55ce3168a65fad11a
Rozhkov Alexey <b0bi@yandex.ru>
2018-05-25 13:39:30 +0300

    Some Facts converted to Theory

--- tests/ClosedXML.Report.Tests/SubrangesTests.cs
+++ tests/ClosedXML.Report.Tests/SubrangesTests.cs
@@ -10,10 +10,12 @@ namespace ClosedXML.Report.Tests
 {
     public class SubrangesTests : XlsxTemplateTestsBase
     {
-        [Fact]
-        public void Simple()
+        [Theory,
+        InlineData("Subranges_Simple_tMD1.xlsx"),
+        InlineData("Subranges_WithSubtotals_tMD2.xlsx")]
+        public void Simple(string templateFile)
         {
-            XlTemplateTest("Subranges_Simple_tMD1.xlsx",
+            XlTemplateTest(templateFile,
                 tpl =>
                 {
                     using (var db = new DbDemos())
@@ -39,40 +41,7 @@ namespace ClosedXML.Report.Tests
                 },
                 wb =>
                 {
-                    CompareWithGauge(wb, "Subranges_Simple_tMD1.xlsx");
-                });
-        }
-
-        [Fact]
-        public void WithSubtotals()
-        {
-            XlTemplateTest("Subranges_WithSubtotals_tMD2.xlsx",
-                tpl =>
-                {
-                    using (var db = new DbDemos())
-                    {
-                        var items = db.items.ToList().GroupBy(i=>i.OrderNo).ToDictionary(x=>x.Key);
-    var parts = db.parts.ToList().ToDictionary(x=>x.PartNo);
-                        customer[] custs = db.customers.LoadWith(x => x.Orders).OrderBy(x=>x.CustNo).ToArray();
-                        foreach (var customer in custs)
-                        {
-                            customer.Orders.Sort((x, y) => x.OrderNo.CompareTo(y.OrderNo));
-                            foreach (var o in customer.Orders)
-                            {
-                                var order = o;
-                                o.Items = items[order.OrderNo].ToList();
-                                o.Items.Sort((x,y)=>x.ItemNo.Value.CompareTo(y.ItemNo));
-                                foreach (var item in o.Items)
-                                    item.Part = parts[item.PartNo.Value];
-                            }
-                        }
-                        //var cust = db.Customers.Include(x => x.Orders.Select(o=>o.Items.Select(i=>i.Part)));
-                        tpl.AddVariable("Customers", custs);
-                    }
-                },
-                wb =>
-                {
-                    CompareWithGauge(wb, "Subranges_WithSubtotals_tMD2.xlsx");
+                    CompareWithGauge(wb, templateFile);
                 });
         }
 
