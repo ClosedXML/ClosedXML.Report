ccea45b8786507a0ed25c382031e84d5c4a9b164
Rozhkov Alexey <b0bi@yandex.ru>
2021-06-20 21:03:54 +0300

    #192 (#207)
    * appveyor. Fix zip name for signed artifacts
    
    * #192 Upgrade DynamicLinq refference

--- tests/ClosedXML.Report.Tests/FormulaEvaluatorTests.cs
+++ tests/ClosedXML.Report.Tests/FormulaEvaluatorTests.cs
@@ -1,6 +1,8 @@
 ﻿using ClosedXML.Report.Utils;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Linq.Dynamic.Core.CustomTypeProviders;
 using System.Linq.Dynamic.Core.Exceptions;
 using System.Linq.Expressions;
 using FluentAssertions;
@@ -97,11 +99,65 @@ namespace ClosedXML.Report.Tests
             eval.Evaluate(@"{{np(b.Manager.Name, null)}}").Should().BeNull();
         }
 
+        [Fact(Skip = "It doesn't work yet")]
+        public void EvalDictionaryParams()
+        {
+            Parameter CreateDicParameter(string name) => new Parameter("item", new Dictionary<string, object>
+                {{"Name", new Dictionary<string, object> {{"FirstName", name }}}});
+
+            var eval = new FormulaEvaluator();
+            eval.Evaluate("{{item.Name.FirstName}}", CreateDicParameter("Julio")).Should().Be("Julio");
+            eval.Evaluate("{{item.Name.FirstName}}", CreateDicParameter("John")).Should().Be("John");
+        }
+
+        [Fact(Skip = "It doesn't work yet")]
+        public void EvalDictionaryParams2()
+        {
+            object CreateDicParameter(string name) => new Dictionary<string, object>
+                {{"Name", new Dictionary<string, object> {{"FirstName", name }}}};
+
+            var config = new ParsingConfig()
+            {
+                CustomTypeProvider = new DefaultDynamicLinqCustomTypeProvider()
+            };
+            var parType = new Dictionary<string, object>().GetType();
+            var lambda = DynamicExpressionParser.ParseLambda(config, new [] {Expression.Parameter(parType, "item")}, typeof(object), "item.Name.FirstName").Compile();
+            lambda.DynamicInvoke(CreateDicParameter("Julio")).Should().Be("Julio");
+            lambda.DynamicInvoke(CreateDicParameter("John")).Should().Be("John");
+        }
+
+        [Fact]
+        public void UsingDynamicLinqTypeTest()
+        {
+            var eval = new FormulaEvaluator();
+            eval.AddVariable("a", "1");
+            eval.Evaluate("{{EvaluateUtils.ParseAsInt(a).IncrementMe()}}").Should().Be(2);
+        }
+
         class Customer
         {
             public int Id { get; set; }
             public string Name { get; set; }
             public Customer Manager { get; set; }
+        }
+    }
+
+    [DynamicLinqType]
+    public static class EvaluateUtils
+    {
+        public static int ParseAsInt(string value)
+        {
+            if (value == null)
+            {
+                return 0;
+            }
+
+            return int.Parse(value);
+        }
+
+        public static int IncrementMe(this int values)
+        {
+            return values + 1;
         }
     }
 }
