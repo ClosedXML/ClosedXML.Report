c4a743948fa0fbba911e8a67222b49c3f206aee2
Jason Finch <jason.finch@gmail.com>
2022-07-01 01:44:51 +1000

    refactor: tests, bump target to netstandard2.0;net462;net6.0, move test data into sqlite. (#274)
    * refactor: tests, bump target to netstandard2.0;net462;net6.0, move test data into sqlite.
    
    - Target netstandard2.0;net462;net6.0 (supported versions) (drop net40;net46)
    - Update testing dependencies
    - Migrate MDB data to sqlite (easier to run multiplatform/CI)
    
    * Task: Update appveyor image to VS2022

---
+++ tests/ClosedXML.Report.Tests/Utils/DatabaseCollection.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace ClosedXML.Report.Tests.Utils
+{
+    [CollectionDefinition("Database")]
+    public class DatabaseCollection : ICollectionFixture<DatabaseFixture>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+}
---
+++ tests/ClosedXML.Report.Tests/Utils/DbSettings.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LinqToDB;
+using LinqToDB.Configuration;
+
+namespace ClosedXML.Report.Tests
+{
+    public class ConnectionStringSettings : IConnectionStringSettings
+    {
+        public string ConnectionString { get; set; }
+        public string Name { get; set; }
+        public string ProviderName { get; set; }
+        public bool IsGlobal => false;
+    }
+
+    public class DbSettings : ILinqToDBSettings
+    {
+        public IEnumerable<IDataProviderSettings> DataProviders => Enumerable.Empty<IDataProviderSettings>();
+        public string DefaultConfiguration => "Default";
+        public string DefaultDataProvider => ProviderName.SQLite;
+
+        private readonly Configuration _configuration = new Configuration();
+
+        public IEnumerable<IConnectionStringSettings> ConnectionStrings
+        {
+            get
+            {
+                yield return
+                    new ConnectionStringSettings
+                    {
+                        Name = "Default",
+                        ProviderName = ProviderName.SQLite,
+                        ConnectionString = _configuration.DefaultConnectionString
+                    };
+            }
+        }
+    }
+}
