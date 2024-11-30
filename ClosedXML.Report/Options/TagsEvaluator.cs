2a89a177fb849d9ed42ce04bac220e4abf2a54bb
jahav <havlicek.honza@gmail.com>
2024-11-30 18:40:38 +0100

    Skip tag evaluation for non-text values
    The bug in SubrangesTests.MultipleSubRanges was caused by setting a value
    of a cell to string.Empty to option tag row that was later sorted as the
    first row instead of last row.
    
    This reduces that chance by parsing tags only from string values. Other
    types can never contains <<tag>> text anyway, so it's fine.

--- ClosedXML.Report/Options/TagsEvaluator.cs
+++ ClosedXML.Report/Options/TagsEvaluator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using ClosedXML.Excel;
@@ -24,16 +25,25 @@ namespace ClosedXML.Report.Options
         /// <returns>Created tags for the cell (may be empty).</returns>
         public OptionTag[] ApplyTagsTo(TemplateCell cell, IXLRange range)
         {
-            OptionTag[] tags;
+            OptionTag[] tags = Array.Empty<OptionTag>();
             if (cell.CellType == TemplateCellType.Formula)
             {
                 tags = Parse(cell.Formula, range, cell, out var newValue);
                 cell.Formula = newValue;
             }
+            else if (cell.CellType == TemplateCellType.Value)
+            {
+                // Only text values can contain tags. Therefore skip all other types.
+                if (cell.Value.TryGetText(out var text))
+                {
+                    tags = Parse(text, range, cell, out var newValue);
+                    cell.Value = newValue == string.Empty ? Blank.Value : newValue;
+                }
+            }
             else
             {
-                tags = Parse(cell.GetString(), range, cell, out var newValue);
-                cell.Value = newValue == "" ? Blank.Value : newValue;
+                // Other template cell types shouldn't even get here
+                cell.Value = Blank.Value;
             }
 
             return tags;
@@ -43,16 +53,15 @@ namespace ClosedXML.Report.Options
         {
             string value = cell.GetString();
             OptionTag[] tags;
-            string newValue;
             var templateCell = new TemplateCell(cell.Address.RowNumber, cell.Address.ColumnNumber, cell);
             if (value.StartsWith("&="))
             {
-                tags = Parse(value.Substring(2), range, templateCell, out newValue);
+                tags = Parse(value.Substring(2), range, templateCell, out var newValue);
                 cell.FormulaA1 = newValue;
             }
             else
             {
-                tags = Parse(value, range, templateCell, out newValue);
+                tags = Parse(value, range, templateCell, out var newValue);
                 cell.Value = newValue;
             }
 
