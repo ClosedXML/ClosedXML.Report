49a1cbdee7c5135067d56a53551c8cf7ed9702d2
jahav <havlicek.honza@gmail.com>
2025-01-07 09:48:53 +0100

    Make skipped test explicit in test explorer

--- tests/ClosedXML.Report.Tests/GroupTagTests.cs
+++ tests/ClosedXML.Report.Tests/GroupTagTests.cs
@@ -75,7 +75,7 @@ namespace ClosedXML.Report.Tests
          InlineData("GroupTagTests_DisableOutline.xlsx"),
          InlineData("GroupTagTests_FormulasInGroupRow.xlsx"),
          InlineData("GroupTagTests_MultiRanges.xlsx"),
-         //InlineData("GroupTagTests_FormulasWithTagsInGroupRow.xlsx"), // Formulas with tags got broken after upgrading to ClosedXML 0.100
+         InlineData("GroupTagTests_FormulasWithTagsInGroupRow.xlsx", Skip = "Formulas with tags got broken after upgrading to ClosedXML 0.100"),
          InlineData("GroupTagTests_TotalLabel.xlsx"),
        ]
         public void Customers(string templateFile)
